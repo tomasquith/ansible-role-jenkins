0da59b5c2b4433bbafb4aad4c5b4ed7156989c09
Tom Asquith <tomasquith@Toms-MacBook-Pro.local>
2018-05-24 16:54:55 +0100

    remove non docker specifics

--- templates/jenkins.sh
+++ templates/jenkins.sh
@@ -1,5 +1,33 @@
 #! /bin/bash -e
 
-JENKINS_WAR="/usr/lib/jenkins/jenkins.war"
+# JENKINS_WAR="/usr/lib/jenkins/jenkins.war"
 
-exec java -Duser.home="{{ jenkins_home }}" "{{ jenkins_java_options }}" -jar ${JENKINS_WAR} "--prefix={{ jenkins_url_prefix }}"
+# exec java -Duser.home="{{ jenkins_home }}" "{{ jenkins_java_options }}" -jar ${JENKINS_WAR} "--prefix={{ jenkins_url_prefix }}"
+
+#! /bin/bash -e
+
+: "${JENKINS_WAR:="/usr/lib/jenkins/jenkins.war"}"
+: "${JENKINS_HOME:="{{ jenkins_home }}"}"
+# touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
+# echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
+# find /usr/share/jenkins/ref/ \( -type f -o -type l \) -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
+
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+
+  # read JAVA_OPTS and JENKINS_OPTS into arrays to avoid need for eval (and associated vulnerabilities)
+  java_opts_array=()
+  while IFS= read -r -d '' item; do
+    java_opts_array+=( "$item" )
+  done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
+
+  jenkins_opts_array=( )
+  while IFS= read -r -d '' item; do
+    jenkins_opts_array+=( "$item" )
+  done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
+
+  exec java -Duser.home="$JENKINS_HOME" "${java_opts_array[@]}" -jar ${JENKINS_WAR} "${jenkins_opts_array[@]}" "$@"
+fi
+
+# As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
+exec "$@"
